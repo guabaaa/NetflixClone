dc9319c5dea1aecec35ae2de6dea382faa4eb5e0
guabaaa <gdandtop950923@gmail.com>
2023-07-27 17:42:06 +0900

    초기세팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { RouterProvider } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+            staleTime: 1000 * 60 * 10,
+            cacheTime: 1000 * 60 * 10,
+        },
+    },
+});
+if (process.env.NODE_ENV == 'production') {
+    console.log = function no_console() {};
+    console.warn = function no_console() {};
+}
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RecoilRoot>
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+        </QueryClientProvider>
+    </RecoilRoot>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
