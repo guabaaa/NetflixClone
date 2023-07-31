ce34924484419d2219e429e9af5c460e4c9cf9c4
guabaaa <gdandtop950923@gmail.com>
2023-07-31 15:07:47 +0900

    test02

--- src/components/global/Container.tsx
+++ src/components/global/Container.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 const Container = () => {
 return (
     <div>
-
+        <Outlet />
     </div>
 )}
 export default Container;
