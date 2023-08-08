0070c482237911ec4e9714d0fe20153b6e7df7cc
guabaaa <gdandtop950923@gmail.com>
2023-08-08 10:54:11 +0900

    navbar 작업 및 css 틀

--- src/components/global/Container.tsx
+++ src/components/global/Container.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import HeaderNavBar from "../../page/navbar/HeaderNavBar";
 
 const Container = () => {
 return (
-    <div>
-        <Outlet />
+    <div className='basicLayout'>
+        <div className='netflix-sans-font-loaded'>
+            <div lang='kor' dir='ltr' data-uia='loc'>
+                <HeaderNavBar />
+                <Outlet />
+            </div>
+        </div>
     </div>
 )}
 export default Container;
